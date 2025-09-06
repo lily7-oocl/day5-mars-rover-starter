f2e2ca46015ea4a641bb0db96ebaf60a191a092b
AYANAMI <2206060753@qq.com>
2025-09-06 13:43:01 +0800

    test:refactor MarRoverTest.java

--- src/test/java/com/afs/tdd/MarRoverTest.java
+++ src/test/java/com/afs/tdd/MarRoverTest.java
@@ -7,9 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MarRoverTest {
     @Test
     public void should_move_when_executeCommand_given_M_and_direction_is_N() {
-        Location ExpectedLocation = new Location(0,1,"N");
+        Location ExpectedLocation = new Location(0,1,Direction.NORTH);
         //Given
-        Location location = new Location(0,0,"N");
+        Location location = new Location(0,0,Direction.NORTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("M");
@@ -18,9 +18,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_M_and_direction_is_E() {
-        Location ExpectedLocation = new Location(1,0,"E");
+        Location ExpectedLocation = new Location(1,0,Direction.EAST);
         //Given
-        Location location = new Location(0,0,"E");
+        Location location = new Location(0,0,Direction.EAST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("M");
@@ -29,9 +29,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_M_and_direction_is_S() {
-        Location ExpectedLocation = new Location(0,-1,"S");
+        Location ExpectedLocation = new Location(0,-1,Direction.SOUTH);
         //Given
-        Location location = new Location(0,0,"S");
+        Location location = new Location(0,0,Direction.SOUTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("M");
@@ -40,9 +40,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_M_and_direction_is_W() {
-        Location ExpectedLocation = new Location(-1,0,"W");
+        Location ExpectedLocation = new Location(-1,0,Direction.WEST);
         //Given
-        Location location = new Location(0,0,"W");
+        Location location = new Location(0,0,Direction.WEST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("M");
@@ -51,9 +51,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_B_and_direction_is_N() {
-        Location ExpectedLocation = new Location(0,-1,"N");
+        Location ExpectedLocation = new Location(0,-1,Direction.NORTH);
         //Given
-        Location location = new Location(0,0,"N");
+        Location location = new Location(0,0,Direction.NORTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("B");
@@ -62,9 +62,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_B_and_direction_is_E() {
-        Location ExpectedLocation = new Location(-1,0,"E");
+        Location ExpectedLocation = new Location(-1,0,Direction.EAST);
         //Given
-        Location location = new Location(0,0,"E");
+        Location location = new Location(0,0,Direction.EAST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("B");
@@ -73,9 +73,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_B_and_direction_is_S() {
-        Location ExpectedLocation = new Location(0,1,"S");
+        Location ExpectedLocation = new Location(0,1,Direction.SOUTH);
         //Given
-        Location location = new Location(0,0,"S");
+        Location location = new Location(0,0,Direction.SOUTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("B");
@@ -84,9 +84,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_B_and_direction_is_W() {
-        Location ExpectedLocation = new Location(1,0,"W");
+        Location ExpectedLocation = new Location(1,0,Direction.WEST);
         //Given
-        Location location = new Location(0,0,"W");
+        Location location = new Location(0,0,Direction.WEST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("B");
@@ -95,9 +95,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_L_and_direction_is_N() {
-        Location ExpectedLocation = new Location(0,0,"W");
+        Location ExpectedLocation = new Location(0,0,Direction.WEST);
         //Given
-        Location location = new Location(0,0,"N");
+        Location location = new Location(0,0,Direction.NORTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("L");
@@ -106,9 +106,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_L_and_direction_is_W() {
-        Location ExpectedLocation = new Location(0,0,"S");
+        Location ExpectedLocation = new Location(0,0,Direction.SOUTH);
         //Given
-        Location location = new Location(0,0,"W");
+        Location location = new Location(0,0,Direction.WEST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("L");
@@ -117,9 +117,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_L_and_direction_is_S() {
-        Location ExpectedLocation = new Location(0,0,"E");
+        Location ExpectedLocation = new Location(0,0,Direction.EAST);
         //Given
-        Location location = new Location(0,0,"S");
+        Location location = new Location(0,0,Direction.SOUTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("L");
@@ -128,9 +128,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_L_and_direction_is_E() {
-        Location ExpectedLocation = new Location(0,0,"N");
+        Location ExpectedLocation = new Location(0,0,Direction.NORTH);
         //Given
-        Location location = new Location(0,0,"E");
+        Location location = new Location(0,0,Direction.EAST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("L");
@@ -139,9 +139,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_R_and_direction_is_N() {
-        Location ExpectedLocation = new Location(0,0,"E");
+        Location ExpectedLocation = new Location(0,0,Direction.EAST);
         //Given
-        Location location = new Location(0,0,"N");
+        Location location = new Location(0,0,Direction.NORTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("R");
@@ -150,9 +150,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_R_and_direction_is_E() {
-        Location ExpectedLocation = new Location(0,0,"S");
+        Location ExpectedLocation = new Location(0,0,Direction.SOUTH);
         //Given
-        Location location = new Location(0,0,"E");
+        Location location = new Location(0,0,Direction.EAST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("R");
@@ -161,9 +161,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_R_and_direction_is_S() {
-        Location ExpectedLocation = new Location(0,0,"W");
+        Location ExpectedLocation = new Location(0,0,Direction.WEST);
         //Given
-        Location location = new Location(0,0,"S");
+        Location location = new Location(0,0,Direction.SOUTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("R");
@@ -172,9 +172,9 @@ class MarRoverTest {
     }
     @Test
     public void should_move_when_executeCommand_given_R_and_direction_is_W() {
-        Location ExpectedLocation = new Location(0,0,"N");
+        Location ExpectedLocation = new Location(0,0,Direction.NORTH);
         //Given
-        Location location = new Location(0,0,"W");
+        Location location = new Location(0,0,Direction.WEST);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("R");
@@ -183,9 +183,9 @@ class MarRoverTest {
     }
     @Test
     public  void should_execute_multiple_command_when_given_MBLR_and_direction_is_N(){
-        Location ExpectedLocation = new Location(0,0,"N");
+        Location ExpectedLocation = new Location(0,0,Direction.NORTH);
         //Given
-        Location location = new Location(0,0,"N");
+        Location location = new Location(0,0,Direction.NORTH);
         MarRover marRover = new MarRover(location);
         //When
         String currentLocation = marRover.executeCommand("MBLR");
