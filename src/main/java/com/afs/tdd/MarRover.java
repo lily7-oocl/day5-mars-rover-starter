8cea91447c8e37c75826ce51beef46ae1d8792c4
lyle.li <lily7@oocl.com>
2025-09-06 12:37:15 +0800

    add:add detail of MarRover.java

--- src/main/java/com/afs/tdd/MarRover.java
+++ src/main/java/com/afs/tdd/MarRover.java
@@ -1,15 +1,44 @@
 package com.afs.tdd;
 
 public class MarRover {
-    private Location location;
+    private final Location location;
+
     MarRover(Location location) {
         this.location = location;
     }
-    public Location executeCommand(String command) {
+
+    public String executeCommand(String command) {
+
+        switch (command) {
+            case "M":
+                move(command);
+            break;
+            case "B":
+                back(command);
+            case "R":
+                turnRight(command);
+                break;
+            case "L":
+                turnLeft(command);
+                break;
+        }
         return null;
 
     }
-    public Location move(String command) {
-        return null;
+
+    public void move(String command) {
+    }
+
+    public void back(String command) {
+    }
+
+    public void turnLeft(String command) {
+    }
+
+    public void turnRight(String command) {
+    }
+
+    public Location getLocation() {
+        return location;
     }
 }
